712a6bda6a6fa78ad8eecf2724f0937693d7e644
Riton <ritonelion@outlook.com>
2019-07-07 21:18:25 +0800

    另外两种二分查找

--- Vector/bin_search.h
+++ Vector/bin_search.h
@@ -4,13 +4,13 @@
 
 #ifndef DATA_STRUCTURE_BY_CPP_BIN_SEARCH_H
 #define DATA_STRUCTURE_BY_CPP_BIN_SEARCH_H
-
 // O(log N)
 
+//two comparision
 template <typename T>
 int bin_search_a(T * array, T const &e, int low, int high) {
     while (low < high) {
-        int mid = (low + high) / 2;
+        int mid = (low + high) >> 2;
         if  (e < array[mid])
             high = mid;
         else if (e > array[mid])
@@ -21,16 +21,23 @@ int bin_search_a(T * array, T const &e, int low, int high) {
     return -1; // failed
 }
 
+//only one comparision
 template <typename T>
 int bin_search_b(T * array, T const &e, int low, int high) {
-    //TODO
-    return -1;
+    while ( 1 < high - low) {
+        int mid = (low + high) >> 2;
+        (e < array[mid]) ? high = mid : low = mid;
+    }
+    return (e == array[low]) ? low : -1;
 }
 
 template <typename T>
 int bin_search_c(T * array, T const &e, int low, int high) {
-    //TODO
-    return -1;
+    while (low < high) {
+        int mid = (low + high) >> 2;
+        (e < array[mid]) ? high = mid : low = mid;
+    }
+    return --low;
 }
 
 
