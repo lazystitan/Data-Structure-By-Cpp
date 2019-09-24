954bf5c25cb3fcfab2299fef359c8c3d4e3caf2c
Riton <ritonelion@outlook.com>
2019-09-24 23:30:40 +0800

    几个算法题

--- InterviewAlgorithms/main_ia.cpp
+++ InterviewAlgorithms/main_ia.cpp
@@ -10,6 +10,7 @@
 using namespace std;
 
 /*
+ * 求只出现一次的数字
  * 使用异或运算符
  * */
 int single_number(vector<int> &nums) {
@@ -23,6 +24,10 @@ void test1() {
     vector<int> v{1,2,2,1,3};
     cout << single_number(v) << endl;
 }
+
+/*
+ * 求众数
+ * */
 
 int majority_element(vector<int> &nums) {
 //    map<int, int> m;
@@ -72,6 +77,10 @@ void test2() {
     vector<int> v{1, 2, 3, 2, 4, 2, 5, 2};
     cout << majority_element(v) << endl;
 }
+
+/*
+ * 给定一矩阵，元素从左到右从上到下为升序，给定目标target，高效判断矩阵中是否存在target
+ * */
 
 bool search_matrix(vector<vector<int>>& matrix, int target) {
 //    最蠢的办法
@@ -161,6 +170,11 @@ void test4() {
     cout << search_matrix(mat, tar) << endl;
 }
 
+/*
+ * 给定两个有序数组nums1和nums2，大小分别为m，n，
+ * 在不使用额外空间的情况下，归并两数组到nums1，假设nums1有足够的空间
+ * */
+
 void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
 //    int p = 0;
 //    int i = 0;
@@ -193,9 +207,149 @@ void test3() {
     }
 }
 
+/*
+ * 判断回文串，只考虑数字和字母，忽略其它字符
+ * */
+
+char to_lower_case(char c) {
+    int gap = 'a' - 'A';
+    if (c < 'a')
+        return c + gap;
+    else
+        return c;
+
+}
+
+bool is_alphabet_or_number(char c) {
+    return ((c <= 'z') && (c >= 'a')) || ((c <= 'Z') && (c >= 'A')) || ((c <= '9') && (c >= '0'));
+}
+
+bool is_palindrome(string s) {
+    int i = 0, j = s.length() - 1;
+    bool fisrt = true;
+    while (i < j) {
+        while (!is_alphabet_or_number(s[i]) && i < s.length())
+            i++;
+
+        if (fisrt) {
+            if (i == s.length()) {
+                cout << "all is not alphabet" << endl;
+                return true;
+            }
+            fisrt = false;
+        }
+
+        while (!is_alphabet_or_number(s[j]) && j >= 0)
+            j--;
+
+        if (to_lower_case(s[i]) != to_lower_case(s[j])) {
+            return false;
+        }
+        i++;
+        j--;
+    }
+
+    return true;
+}
+
+
+
+void test5() {
+    string s = "A man, a plan, a canal: Panama";
+    cout << (is_palindrome(s) ? "true" : "false") << endl;
+    string s2 = "race a car";
+    cout << (is_palindrome(s2) ? "true" : "false") << endl;
+    string s3 = "''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''";
+    cout << (is_palindrome(s3) ? "true" : "false") << endl;
+    string s4 = "''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''";
+    cout << (is_palindrome(s4) ? "true" : "false") << endl;
+    string s5 = "0P";
+    cout << (is_palindrome(s5) ? "true" : "false") << endl;
+}
+
+
+/*
+ * 将一个字符串划分为多个回文串，要求列出所有结果
+ * */
+vector<vector<string>> partition(string s) {
+    vector<vector<string>> v;
+    //TODO
+    return v;
+}
+
+ostream& operator<<(ostream &os, const vector<vector<string>> &answer) {
+    for (const auto &v : answer) {
+        cout << "[";
+        for (const auto &vs : v) {
+            cout << vs << ",";
+        }
+        cout << "]" << endl;
+    }
+    return os;
+}
+
+void test6() {
+    string s = "aab";
+    vector<vector<string>> v{{"aa", "b"}, {"a", "a", "b"}};
+    cout << v;
+
+}
+
+/*
+ * 反转字符数组
+ * */
+
+void reverse_string(vector<char> &s) {
+    int front = 0, back = s.size() - 1;
+    while (front < back) {
+//        太慢
+//        char temp = s[front];
+//        s[front] = s[back];
+//        s[back] = temp;
+
+/*
+ *  bit0 = 0, bit1 = 1;
+ *  bit0 = bit0 ^ bit1 = 1;
+ *  bit1 = bit0 ^ bit1 = 0;
+ *  bit0 = bit0 ^ bit1 = 1;
+ */
+
+//        s[front] = s[front] ^ s[back];
+//        s[back] = s[front] ^ s[back];
+//        s[front] = s[front] ^ s[back];
+
+        swap(s[front++], s[back--]);
+//        front++;
+//        back--;
+    }
+}
+
+void test7() {
+    vector<char> v{'h', 'e', 'l', 'l', 'o'};
+    reverse_string(v);
+    for (const auto &c : v) {
+        cout << c << " ";
+    }
+    cout << endl;
+}
+
+/*
+ * 找到第一个不重复出现的字符的索引
+ */
+
+int first_unique_char(string s) {
+
+
+
+    return 0;
+}
+
 int main() {
 //    test1();
 //    test2();
 //    test3();
-    test4();
+//    test4();
+//    test5();
+//    test6();
+    test7();
 }
