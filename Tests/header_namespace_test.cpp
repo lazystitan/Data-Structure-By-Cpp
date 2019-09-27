24f67406477b130f887ab46293f4a33f8b283b7b
Riton <ritonelion@outlook.com>
2019-09-28 00:23:40 +0800

    上次没加代码，日

---
+++ Tests/header_namespace_test.cpp
@@ -0,0 +1,10 @@
+//
+// Created by Riton on 2019/9/27.
+//
+
+#include "header_namespace_test.h"
+
+
+int main() {
+    std::vector<int> v;
+}
---
+++ Tests/header_namespace_test.h
@@ -0,0 +1,10 @@
+//
+// Created by Riton on 2019/9/27.
+//
+
+#ifndef DATA_STRUCTURE_BY_CPP_HEADER_NAMESPA_TEST_H
+#define DATA_STRUCTURE_BY_CPP_HEADER_NAMESPA_TEST_H
+
+#include <vector>
+
+#endif //DATA_STRUCTURE_BY_CPP_HEADER_NAMESPA_TEST_H
